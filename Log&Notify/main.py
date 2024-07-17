628bc3a9c6021b308f4d801786be8511ae409876
namnnhe <namnnhe163790@gmail.com>
2024-07-17 17:22:14 +0700

    rewrite main

--- Log&Notify/main.py
+++ Log&Notify/main.py
@@ -2,6 +2,8 @@ import threading
 import time
 from filesystem_monitor import monitor_filesystem
 from db import create_database  # Import hàm tạo database
+from auth_log import create_fake_auth_logs
+from shell_log import create_fake_shell_logs
 
 # --- Tùy chọn ---
 ENABLE_LOG_MONITORING = False 
@@ -11,7 +13,8 @@ def show_menu():
     """Hiển thị menu tùy chọn."""
     print("\n--- Menu ---")
     print("1. Bật/Tắt theo dõi log")
-    print("2. Thoát")
+    print("2. Tạo file log theo số lượng")
+    print("3. Thoát")
 
 def handle_choice(choice):
     """Xử lý lựa chọn từ menu."""
@@ -29,6 +32,18 @@ def handle_choice(choice):
             # Dừng theo dõi nếu tắt
             STOP_MONITORING = True
     elif choice == '2':
+            quantity_auth = int(input("Nhập số lượng log giả auth muốn tạo: "))
+            quantity_shell = int(input("Nhập số lượng log giả shell muốn tạo: "))
+
+            # Tạo thread cho mỗi loại log
+            auth_thread = threading.Thread(target=create_fake_auth_logs, args=(quantity_auth,))
+            shell_thread = threading.Thread(target=create_fake_shell_logs, args=(quantity_shell,))
+
+            # Chạy các thread
+            auth_thread.start()
+            shell_thread.start()
+    
+    elif choice == '3':
         print("Thoát chương trình...")
         STOP_MONITORING = True
         # Đợi thread theo dõi kết thúc (nếu đang chạy)
